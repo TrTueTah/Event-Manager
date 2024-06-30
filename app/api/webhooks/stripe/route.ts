0567675bd9057ac0274c855a206a0aa737e04a67
TrTueTah <henrytsuki@gmail.com>
2024-06-30 20:37:20 +0700

    final

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import { createOrder } from "@/lib/actions/order.actions";
 
 export async function POST(request: Request) {
+  console.log("Webhook received");
   const body = await request.text();
 
   const sig = request.headers.get("stripe-signature") as string;
